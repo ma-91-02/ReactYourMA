6971d0d4f29fe642f5091b49b9c3a6f274b62c7c
mahmood <mahmood@mgt.eu>
2022-10-03 08:41:56 +0300

    add multi lang

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './images/logo.png';
 import './App.css';
+import SelectLang from './components/selecteLang/SelectLang';
+import { useTranslation } from "react-i18next";
+
 
 function App() {
+  const { t } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      <h1>Weclome to Your MA coming soon</h1>
+      <h1>{t("welcome")} {t("your")} {t("MA")}</h1>
+      <SelectLang/>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next'
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+i18next
+  .use(HttpApi)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ['en', 'ar', 'ru'],
+    fallbackLng: 'en',
+    debug: false,
+    // Options for language detector
+    detection: {
+      order: ['path', 'cookie', 'htmlTag'],
+      caches: ['cookie'],
+    },
+    // react: { useSuspense: false },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+  })
+
+const loadingMarkup = (
+  <div className="py-4 text-center">
+    <h3>Loading..</h3>
+  </div>
+)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Suspense fallback={loadingMarkup}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Suspense>,
 );
 
 // If you want to start measuring performance in your app, pass a function
