b7654a725daf67b3c135815f8094c102924756b6
mahmood <mahmood@mgt.eu>
2022-10-03 07:51:22 +0300

    update

--- src/components/Logo/Logo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import './Logo.scss';
-import pic from '../../images/logo.png';
-
-const logo = props => <div className="logo" ><img className='logotop' src={pic}/></div>;
-
-export default logo;
--- src/index.js
+++ src/index.js
@@ -1,53 +1,17 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import i18next from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import HttpApi from 'i18next-http-backend'
-import LanguageDetector from 'i18next-browser-languagedetector'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import App from "./App";
-import 'bootstrap/dist/js/bootstrap.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'flag-icon-css/css/flag-icon.min.css'
-import "./index.scss";
 
-i18next
-  .use(HttpApi)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    supportedLngs: ['en', 'ar', 'ru'],
-    fallbackLng: 'en',
-    debug: false,
-    // Options for language detector
-    detection: {
-      order: ['path', 'cookie', 'htmlTag'],
-      caches: ['cookie'],
-    },
-    // react: { useSuspense: false },
-    backend: {
-      loadPath: '/assets/locales/{{lng}}/translation.json',
-    },
-  })
-
-const loadingMarkup = (
-  <div className="py-4 text-center">
-    <h3>Loading..</h3>
-  </div>
-)
-
-ReactDOM.render(
-  <Suspense fallback={loadingMarkup}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </Suspense>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
