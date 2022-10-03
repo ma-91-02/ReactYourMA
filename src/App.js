8613f9e142c862e6dd488b323326f610858f9aef
mahmood <mahmood@mgt.eu>
2022-10-03 10:41:11 +0300

    upload other parts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import {Route ,Switch, Redirect, withRouter} from 'react-router-dom';
+import { withRouter} from 'react-router-dom';
 
 import Layout from "./components/Layout/Layout";
 import Backdrop from "./components/Backdrop/Backdrop";
