c7eb02a185d2e1dd7d3976fcb2aa1151e0abc79d
mahmood <mahmood@mgt.eu>
2022-10-04 15:47:21 +0300

    add change some Link

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "./NavigationItems.css";
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 // import styles from "./Landing.module.css";
 import pic from "../images/logo.png";
 
